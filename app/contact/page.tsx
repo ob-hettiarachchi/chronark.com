b8dedb01f5e4cef677c2090192fd94eef55f4610
OB Hettiarachchi <osanda.buwaneka@gmail.com>
2023-10-08 07:09:22 +0530

    init

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/OBHettiarachchi",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@OBHettiarachchi",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:osanda.buwaneka@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "osanda.buwaneka@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/ob-hettiarachchi",
 		label: "Github",
-		handle: "chronark",
+		handle: "ob-hettiarachchi",
 	},
 ];
 
@@ -46,7 +46,7 @@ export default function Example() {
 									{s.icon}
 								</span>{" "}
 								<div className="z-10 flex flex-col items-center">
-									<span className="text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+									<span className="text-xl font-small duration-150 lg:text-xl text-zinc-200 group-hover:text-white font-display">
 										{s.handle}
 									</span>
 									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
